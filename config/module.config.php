a67f5b03b40c6031780c3a340ed813873e35fa67
Tim Hollies <tim.hollies@warwick.ac.uk>
2016-11-29 15:32:19 +0000

    Updated config

--- config/module.config.php
+++ config/module.config.php
@@ -1,9 +1,18 @@
 <?php
 
-return array(
-    'service_manager' => array(
-        'factories' => array(
-            'Omeka\AuthenticationService' => 'LDAP\Service\AuthenticationServiceFactory'
-        )
-    )
-);
+namespace LDAP;
+
+use Omeka\AuthenticationService as OmekaAuthenticationService;
+use Zend\Authentication\AuthenticationService as ZendAuthenticationService;
+
+return [
+    // Add this section:
+    'service_manager' => [
+        'aliases' => [            
+            ZendAuthenticationService::class => Service\AuthenticationServiceFactory::class
+        ],
+        'factories' => [
+            OmekaAuthenticationService::class => Service\AuthenticationServiceFactory::class
+        ],
+    ]
+];
